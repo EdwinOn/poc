0249bdabe59f9313a166948795e885568366cda3
EdwinOn <99795601+EdwinOn@users.noreply.github.com>
2022-03-08 14:22:42 +0100

    Weryfkacja poc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormioAppConfig, FormioModule } from "angular-formio";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { AppConfig } from "../config";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [BrowserModule, FormioModule],
+  imports: [BrowserModule, FormioModule, HttpClientModule],
   declarations: [AppComponent],
   providers: [{ provide: FormioAppConfig, useValue: AppConfig }],
   bootstrap: [AppComponent]
